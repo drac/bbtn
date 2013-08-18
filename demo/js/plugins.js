014a84b4b6df22fa5c527001629bad47b1807189
drac <seyhan.drc@gmail.com>
2013-08-18 16:45:35 +0300

    Download count

--- demo/js/plugins.js
+++ demo/js/plugins.js
@@ -21,4 +21,9 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+// Google Analytics: change UA-XXXXX-X to be your site's ID.
+var _gaq=[['_setAccount','UA-43028771-1'],['_trackPageview']];
+(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+g.src='//www.google-analytics.com/ga.js';
+s.parentNode.insertBefore(g,s)}(document,'script'));
+
